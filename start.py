89ff927d2b14af8f18267fa8b1240d1f626e9415
Brady Moon <bradygmoon@gmail.com>
2023-06-14 15:13:36 -0400

    Change arrow widths, start zoomed in more, and changed animation order of beginning points.

--- start.py
+++ start.py
@@ -31,36 +31,40 @@ class trochoids(Scene):
         start = Dot().move_to(dubins.get_points()[0])
         start.set_color("#000000")
         start.set_stroke(width=1)
-        slabel = Tex("Start").next_to(start, UP, buff=0.3)
+        slabel = Tex("Start").next_to(start, direction=np.array([-1., 1., 0.]), buff=0.25)
         slabel.set_color("#000000")
         slabel.scale(1.0)
 
         goal = Dot().move_to(dubins.get_points()[-1])
         goal.set_color("#000000")
         goal.set_stroke(width=1)
-        glabel = Tex("Goal").next_to(goal, UP, buff=0.5)
+        glabel = Tex("Goal").next_to(goal, direction=np.array([-1., 1., 0.]), buff=0.25)
         glabel.set_color("#000000")
         glabel.scale(1.0)
 
 
-        self.camera.frame.set_width(50)
-        self.camera.frame.move_to(dubins.get_center())
+        self.camera.frame.set_width(dubins.get_width()*4)
         
-        self.play(ShowCreation(start),ShowCreation(slabel),ShowCreation(goal),ShowCreation(glabel))
-        self.wait()
-        self.play(ShowCreation(dubins))
+        self.camera.frame.move_to(dubins.get_center())
+        self.camera.frame.set_z
+        
+        
         #Frame 1
         #Create start and goal 
         (x0,y0,z0) = dubins.get_points()[0]
         (x1,y1,z1) = dubins.get_points()[-1]
 
-        svec = Arrow(start=[x0,y0,0],end=[x0+0.5,y0-0.40,0],buff=0,stroke_color="#000000",stroke_width=2)
-        gvec = Arrow(start=[x1,y1,0],end=[x1+0.5,y1-0.2,0],buff=0,stroke_color="#000000",stroke_width=2)
-        self.play(ShowCreation(svec),ShowCreation(gvec))
+        svec = Arrow(start=[x0,y0,0],end=[x0+0.5,y0-0.40,0],buff=0,stroke_color="#000000",stroke_width=3)
+        gvec = Arrow(start=[x1,y1,0],end=[x1+0.5,y1-0.2,0],buff=0,stroke_color="#000000",stroke_width=3)
+        self.play(ShowCreation(start),ShowCreation(svec))
+        self.play(ShowCreation(slabel))
+        self.play(ShowCreation(goal), ShowCreation(gvec))
+        self.play(ShowCreation(glabel))
         self.wait()
-        #Frame 2 
+        self.play(ShowCreation(dubins))
+        self.wait()
         
-        #Frame 3
+        #Frame 2
         grid = NumberPlane(x_range=(-15, 15, 1), y_range=(-15, 15, 1),background_line_style={"stroke_width": 0.5,"stroke_opacity": 0.0})
 
         def wind_field(x, y):
@@ -74,27 +78,27 @@ class trochoids(Scene):
         vector_field.set_height(80)
         self.play(ShowCreation(vector_field))
 
-        #Frame 4
+        #Frame 3
         moved = Dot().move_to(dubins2.get_points()[-1])
         moved.set_stroke(width=1)
         moved.set_color("#000000")
         mlabel = Tex("Moved","\\,","Goal").next_to(moved, UP, buff=0.3)
         mlabel.set_color("#000000")
         (x2,y2,z2) = dubins2.get_points()[-1]
-        mvec = Arrow(start=[x2,y2,0.0],end=[x2+0.5,y2-0.2,0.0],buff=0,stroke_color="#000000",stroke_width=2)
+        mvec = Arrow(start=[x2,y2,0.0],end=[x2+0.5,y2-0.2,0.0],buff=0,stroke_color="#000000",stroke_width=3)
         mvec.set_color("#000000")
         mlabel.scale(1.0)
         self.play(ShowCreation(moved),ShowCreation(mlabel),ShowCreation(mvec))
         self.play(ShowCreation(dubins2))
         self.wait()
 
-        #Frame 6 (Show rotation) 
+        #Frame 4 (Show rotation) 
         self.play(Uncreate(dubins),Uncreate(slabel),Uncreate(glabel),Uncreate(mlabel),Uncreate(dubins2))
         objs = VGroup(vector_field,start,goal,svec,gvec,mvec,moved,dubins)
         self.play(Rotate(objs,-PI/8))
 
 
-        #Frame 7 (Remove vector field)
+        #Frame 5 (Remove vector field)
         self.play(Uncreate(vector_field))
         #Draw mini graphs on to dots 
         sgrid = NumberPlane(x_range=(-1, 1, 1),y_range=(-1, 1, 1),background_line_style={"stroke_width": 1, "stroke_opacity": 0.0},)
